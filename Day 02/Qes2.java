693cd3404147dbe563d712e6ea663b012c93cd8a
Badass Nemesis <harshitanant29@gmail.com>
2022-11-13 15:01:23 +0530

    :)

--- Day 02/Qes2.java
+++ Day 02/Qes2.java
@@ -6,14 +6,17 @@ public class Qes2 {
     public static String columnWalaName;
 
     public static void dumbRecursion(int columnNumber) {
-        if (columnNumber < 1) {
+        if (columnNumber <= 1) {
             return;
         }
         int remainder = columnNumber % 26;
+        // System.out.println(remainder); // debugging
+        
         if (remainder == 0) {
             columnWalaName = "Z" + columnWalaName;
+            columnNumber--; // ye minus is very important. agar ni likhenge to fir ek extra letter add ho
+                            // jayega column name mein
         } else {
-            System.out.println(remainder);
             columnWalaName = (char) ('A' + remainder - 1) + columnWalaName;
         }
 
